6b2c17cda43ac0271b949b10c52f62f63667ef4d
Ibrahim Sy <ibrahim93ite@gmail.com>
2020-05-26 18:36:49 +0200

    we add lambda exprission to the program

--- TestGitHub/Program.cs
+++ TestGitHub/Program.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using static System.Console;
 namespace TestGitHub
 {
     class Program
     {
-        //welcome to C#
+        //Lambda Exprission
+        delegate int lamdaDelegate(int x);
         static void Main(string[] args)
         {
+            int[] arr = { 3, 21, 4, 8, 10, 11, 18 };
+            lamdaDelegate ld = x => (x > 10) ? x : -1;
+            Func<int, int> moreThanTen = x => (x > 10) ? x : -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (moreThanTen.Invoke(arr[i]) == -1)
+                    continue;
+                WriteLine(moreThanTen.Invoke(arr[i]));
+            }
         }
     }
 }
